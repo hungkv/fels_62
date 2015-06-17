249d5c6298f51fd2a5cf3d71c0f3583e82d2dd07
Phan Thanh Giang <phan.thanh.giang@framgia.com>
2015-06-17 14:47:23 +0700

    show categories and create lesson with random 20 words

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,15 @@
 class Lesson < ActiveRecord::Base
   has_many :lesson_words
-  has_many :words, through :lesson_words
-  belongs_to :user  
+  has_many :words, through: :lesson_words
+  belongs_to :user
+  after_create :random_20_words
+
+  private
+  def random_20_words
+    category = Category.find self.category_id
+    category.words.sample(20).each do |word|
+      lesson_word = self.lesson_words.build word_id: word.id
+      lesson_word.save
+    end
+  end
 end
