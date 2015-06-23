2bf0d8e34ac9a01123fb0c8cfe77e286773e891b
Phan Thanh Giang <phan.thanh.giang@framgia.com>
2015-06-23 14:04:49 +0700

    Rspec model user

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    pass =  {Faker::Internet.password}
+    name {Faker::Name.name}
+    email {Faker::Internet.safe_email}
+    password pass
+    password_confirmation pass
+  end
+end
